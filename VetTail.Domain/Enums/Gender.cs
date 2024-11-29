2e01490f0a0ffc476979bad63a3f1a03d51453ca
Traam <younesMRX@outlook.com>
2024-11-29 20:31:53 +0100

    [Added Enums] Gender and User Role enums

---
+++ VetTail.Domain/Enums/Gender.cs
@@ -0,0 +1,7 @@
+﻿namespace VetTail.Domain.Enums;
+
+public enum Gender
+{
+    Male,
+    Female
+}
---
+++ VetTail.Domain/Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace VetTail.Domain.Enums;
+
+public enum Role
+{
+    StaffMember = 1,
+    Veterinarian = StaffMember << 1,
+    Owner = Veterinarian << 1
+}
