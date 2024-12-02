22845536628febcbb87efcb93fc699683592a993
Traam <younesMRX@outlook.com>
2024-12-02 11:49:54 +0100

    [Added] [Infrastructure] created Application db context class has users db set

---
+++ VetTail.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using VetTail.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace VetTail.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
