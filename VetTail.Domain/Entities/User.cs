73562aebad30655f6d0ee2be62f15d365f2a1d38
Traam <younesMRX@outlook.com>
2024-12-02 12:40:28 +0100

    [Added Entity] User class

---
+++ VetTail.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using VetTail.Domain.Common.Attributes;
+using VetTail.Domain.Common.Abstractions;
+using VetTail.Domain.Enums;
+using System;
+
+namespace VetTail.Domain.Entities;
+
+public class User : AuditableEntity
+{
+    public ulong Id { get; set; }
+
+    [AuditableProperty] public required string Email { get; set; }
+    [AuditableProperty] public required string Username { get; set; }
+    [AuditableProperty] public required string Password { get; set; }
+    [AuditableProperty] public Role Role { get; set; }
+    [AuditableProperty] public required string CIN { get; set; }
+    public Gender Gender { get; set; }
+
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+    public string? Phone { get; set; }
+    public string? Image { get; set; }
+
+    public DateOnly Birthdate { get; set; }
+
+    public new  byte[] Hash => Hash();
+}
