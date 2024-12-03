1f878f1da31c651186f3c082fae05f9bcdb16fc7
Traam <younesMRX@outlook.com>
2024-12-03 16:43:35 +0100

    [Added] [Domain] Repository and unit of work interfaces

---
+++ VetTail.Domain/Common/Intefaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using VetTail.Domain.Common.Abstractions;
+
+namespace VetTail.Domain.Common.Intefaces;
+
+public interface IRepository<TEntity, in TKey> where TEntity : Entity where TKey : notnull
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellation = default);
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellation = default);
+    Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancel = default);
+    Task<TEntity> DeleteAsync(TEntity entity,CancellationToken cancellationToken = default);
+}
---
+++ VetTail.Domain/Common/Intefaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace VetTail.Domain.Common.Intefaces;
+
+public interface IUnitOfWork
+{
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
