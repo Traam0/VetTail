abb65c3b45d8879fb446552653a3631e669445ec
Traam <younesMRX@outlook.com>
2024-12-03 16:54:03 +0100

    [Added] [Interceptors] Added database context intercpetors for both adutitable entitty and entity events

---
+++ VetTail.Infrastructure/Data/Interceptors/AuditablePropertyInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using VetTail.Domain.Common.Abstractions;
+
+namespace VetTail.Infrastructure.Data.Interceptors;
+
+public class AuditablePropertyInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        this.InterceptEntities(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        this.InterceptEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    void InterceptEntities(DbContext? context) {
+        if (context is null) return;
+        Span<EntityEntry<AuditableEntity>> entries = CollectionsMarshal.AsSpan(context.ChangeTracker.Entries<AuditableEntity>().ToList());
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            EntityEntry<AuditableEntity> entry = entries[i];
+            
+            if(entry.State is EntityState.Added or EntityState.Modified)
+            {
+                DateTimeOffset currentTime = DateTimeOffset.UtcNow;
+                if (entry.State is EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = currentTime;
+                }
+                
+                entry.Entity.UpdatedAt = currentTime;
+            }
+        }
+
+    }
+}
---
+++ VetTail.Infrastructure/Data/Interceptors/EventsDispatcherInterceptor.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VetTail.Domain.Common.Abstractions;
+using System.Linq;
+
+namespace VetTail.Infrastructure.Data.Interceptors;
+
+public class EventsDispatcherInterceptor(IMediator mediator) : SaveChangesInterceptor
+{
+    private readonly IMediator mediator = mediator;
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchEvents(eventData.Context).GetAwaiter().GetResult();
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        await DispatchEvents(eventData.Context);
+
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public async Task DispatchEvents(DbContext? context)
+    {
+        if (context is null) return;
+
+        IEnumerable<Entity> entities = context.ChangeTracker.Entries<Entity>()
+            .Where(x => x.Entity.Events.Any())
+            .Select(x => x.Entity);
+
+
+        IEnumerable<Event> events = entities.SelectMany(x => x.Events);
+
+        foreach (Event @event in events) {
+            await mediator.Publish(@event);
+        }
+        
+        entities.ToList().ForEach(x => x.ClearEvents());
+    }
+}
