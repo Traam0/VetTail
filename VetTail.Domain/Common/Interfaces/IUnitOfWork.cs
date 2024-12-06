d568ea27408078eb188e8f1312a7f5585158887f
Traam <younesMRX@outlook.com>
2024-12-06 10:07:00 +0100

    ✨ feat<domain>: add IRepository and IUnitOfWork interfaces Foundational interfaces for repository and unit of work patterns to manage data access and transactions.

---
+++ VetTail.Domain/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace VetTail.Domain.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    bool SaveChanges();
+    Task<bool> SaveChangeAsync(CancellationToken cancellationToken);
+
+}
