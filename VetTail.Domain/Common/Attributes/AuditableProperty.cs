ab80dc466764d665eb2458ee85feb78b55c914fd
Traam <younesMRX@outlook.com>
2024-12-06 10:04:15 +0100

    ✨ feat<domain>: add AuditableProperty attribute for tracking entity changes

---
+++ VetTail.Domain/Common/Attributes/AuditableProperty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VetTail.Domain.Common.Attribues;
+
+[AttributeUsage(AttributeTargets.Property)]
+internal class AuditableProperty : Attribute
+{
+    public string? AltName { get; set; }
+
+    public AuditableProperty()
+    {
+        this.AltName = null;
+    }
+
+    public AuditableProperty(string altName)
+    {
+        this.AltName = altName;
+    }
+}
