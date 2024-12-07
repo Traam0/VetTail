dfd8997c45506fd21ded06b6a36f13665f6ba553
Traam <younesMRX@outlook.com>
2024-12-07 18:17:50 +0100

    ✨ feat<infrastructure>: implement initial ApplicationDbContext class  Added the ApplicationDbContext class with initial setup for database context in the Infrastructure project.

---
+++ VetTail.Infrastructure/Data/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VetTail.Domain.Entities;
+
+namespace VetTail.Infrastructure.Data.Persistence;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<ClientNotes> ClientNotes { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<PetNotes> PetNotes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);  
+    }
+}
