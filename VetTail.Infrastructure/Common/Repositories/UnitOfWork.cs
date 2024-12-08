a7c2090d38a4a56bc4198197353a7e6a942e0816
Traam <younesMRX@outlook.com>
2024-12-08 15:22:56 +0100

    ✨ feat<infrastructure>: implement IUnitOfWork and IRepository Interfaces from domain layer Added base implementations of IUnitOfWork and IRepository to handle data access and transaction management.

---
+++ VetTail.Infrastructure/Common/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using VetTail.Domain.Common.Interfaces;
+using VetTail.Infrastructure.Data.Persistence;
+
+namespace VetTail.Infrastructure.Common.Repositories;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext context;
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        this.context = context; 
+    }
+
+    public async Task<bool> SaveChangeAsync(CancellationToken cancellationToken)
+    {
+        return await context.SaveChangesAsync(cancellationToken) != 0;
+    }
+
+
+    public bool SaveChanges()
+    {
+        return context.SaveChanges() != 0;
+    }
+}
