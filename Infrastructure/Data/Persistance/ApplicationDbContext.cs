0567916c839dd286fe2dcb44a7b1617342f3505d
Traam <younesMRX@outlook.com>
2024-12-12 05:04:36 +0100

    ✨ feat<infrastructure>: set up EF Core with initial ApplicationDbContext Installed Entity Framework Core dependencies and implemented the foundational ApplicationDbContext for database integration.

---
+++ Infrastructure/Data/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VetTail.Domain.Entities;
+
+namespace VetTail.Infrastructure.Data.Persistance;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
