c4e719aea1c332451a9c5bb4232865f43ec4f282
Traam <younesMRX@outlook.com>
2024-12-12 05:33:44 +0100

    ♻️ refactor<domain>: update AuditableProperty to a sealed class Refined AuditableProperty to be a sealed class, ensuring it cannot be inherited.

--- Domain/Common/Attributes/AuditableProperty.cs
+++ Domain/Common/Attributes/AuditableProperty.cs
@@ -3,7 +3,7 @@
 namespace VetTail.Domain.Common.Attributes;
 
 [AttributeUsage(AttributeTargets.Property)]
-public class AuditableProperty : Attribute
+public sealed class AuditableProperty : Attribute
 {
     public string? AltName { get; init; }
 
