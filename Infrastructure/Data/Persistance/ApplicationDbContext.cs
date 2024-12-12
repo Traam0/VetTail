85cf3a22b8c5a341ba202cf5af99056c8d2e6a29
Traam <younesMRX@outlook.com>
2024-12-12 05:20:34 +0100

    ♻️ refactor<infrastructure>: update ApplicationDbContext to use configuration classes Refactored ApplicationDbContext to utilize entity configuration classes.

--- Infrastructure/Data/Persistance/ApplicationDbContext.cs
+++ Infrastructure/Data/Persistance/ApplicationDbContext.cs
@@ -6,4 +6,10 @@ namespace VetTail.Infrastructure.Data.Persistance;
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
