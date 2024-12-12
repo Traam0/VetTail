fe6ec0ac8715f51b402f755fd094cf562a646c36
Traam <younesMRX@outlook.com>
2024-12-12 05:05:24 +0100

    ✨ feat<infrastructure>: add DIContainerRegistry and register ApplicationDbContext Introduced DIContainerRegistry to centralize dependency registration and configured ApplicationDbContext in the DI container.

---
+++ Infrastructure/DIContainerRegistery.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using VetTail.Infrastructure.Data.Persistance;
+
+namespace VetTail.Infrastructure;
+
+public static partial class DIContainerRegistery
+{
+    public static IServiceCollection RegisterInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+
+        services.RegisterDataBaseContxet(configuration);
+        return services;
+    }
+
+    private static IServiceCollection RegisterDataBaseContxet(this IServiceCollection services, ConfigurationManager configuration) 
+    {
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            string? connectionString = configuration.GetConnectionString("SqlServer") ;
+
+            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentException("missing connection string: SqlServer from");
+
+            options.UseSqlServer(connectionString);
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+        });
+
+        return services;
+    }
+}
