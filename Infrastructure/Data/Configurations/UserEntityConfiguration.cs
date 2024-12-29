70a441f046cb19153ca7e22054337c62d5468913
Traam <younesMRX@outlook.com>
2024-12-29 17:00:17 +0100

    ♻️ refactor<infrastructure>: update UserEntityConfiguration to align with updated User class  Refactored UserEntityConfiguration to match the new structure of the User class, reflecting changes from IdentityUser inheritance.

--- Infrastructure/Data/Configurations/UserEntityConfiguration.cs
+++ Infrastructure/Data/Configurations/UserEntityConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using VetTail.Domain.Entities;
 using VetTail.Domain.Enums;
 
@@ -12,70 +13,7 @@ public sealed class UserEntityConfiguration : IEntityTypeConfiguration<User>
         builder.ToTable("vet_users");
 
         builder.HasKey(t => t.Id)
-            .HasName("pk_vet_users_id");
-
-        builder.Property(u => u.Id)
-            .HasColumnName("_id")
-            .ValueGeneratedOnAdd();
-
-        builder.Property(u => u.FirstName)
-            .HasColumnName("first_name")
-            .IsRequired();
-
-        builder.Property(u => u.LastName)
-            .HasColumnName("last_name")
-            .IsRequired();
-
-        builder.Property(u => u.CIN)
-            .HasColumnName("cin")
-            .IsRequired();
-
-        builder.Property(u => u.Phone)
-            .HasColumnName("phone_number")
-            .IsRequired();
-
-        builder.Property(u => u.Gender)
-            .HasColumnName("gender")
-            .IsRequired()
-            .HasDefaultValue(Gender.Unknown);
-
-        builder.Property(u => u.Username)
-            .HasColumnName("username")
-            .IsRequired();
-
-        builder.Property(u => u.Password)
-            .HasColumnName("password")
-            .IsRequired();
-
-        builder.Property(u => u.Role)
-            .HasColumnName("role")
-            .IsRequired()
-            .HasDefaultValue(Role.Staff);
-
-        builder.Property(u => u.Status)
-            .HasColumnName("account_status")
-            .IsRequired()
-            .HasDefaultValue(AccountStatus.Active);
-
-        builder.Property(u => u.Email)
-            .HasColumnName("email_address")
-            .IsRequired(false)
-            .HasDefaultValue(null);
-
-        builder.Property(u => u.Image)
-            .HasColumnName("image")
-            .IsRequired(false)
-            .HasDefaultValue(null);
-
-        builder.Property(u => u.DateOfBirth)
-            .HasColumnName("date_of_birth")
-            .IsRequired();
-
-        builder.Property(u => u.Hash)
-            .HasColumnName("sec_hash")
-            .HasComment("security hash to keep track of row version and invalidate sessions")
-            .IsRequired(false)
-            .HasDefaultValue(null);
+            .HasName("pk-vet_users-id");
 
 
         builder.Property(u => u.CreatedAt)
@@ -87,9 +25,8 @@ public sealed class UserEntityConfiguration : IEntityTypeConfiguration<User>
             .IsRequired(false);
 
 
-        builder.HasIndex(u => u.CIN, "unique_vet_users_cin").IsUnique();
-        builder.HasIndex(u => u.Username, "unique_vet_users_username").IsUnique();
-        builder.HasIndex(u => u.Email, "unique_vet_users_email_address").IsUnique();
-        builder.HasIndex(u => u.Phone, "unique_vet_users_phone_number");
+        //builder.HasIndex(u => u.CIN, "unique_vet_users_cin").IsUnique();
+        builder.HasIndex(u => u.UserName, "unique-vet_users_username").IsUnique();
+        builder.HasIndex(u => u.Email, "unique-vet_users_email_address").IsUnique();
     }
 }
