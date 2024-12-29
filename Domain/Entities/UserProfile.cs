c1153ee57913efdfc4026743fcdf597f7d161388
Traam <younesMRX@outlook.com>
2024-12-29 16:57:47 +0100

    ♻️ refactor<domain>: make User extend IdentityUser, add UserProfile, and integrate MediatR & AutoMapper Refactored User to inherit from IdentityUser, introduced UserProfile entity with a relationship to User, and added dependencies for MediatR, AutoMapper, and Microsoft.AspNetCore.Identity.EntityFrameworkCore

---
+++ Domain/Entities/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VetTail.Domain.Enums;
+
+namespace VetTail.Domain.Entities;
+
+public class UserProfile
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string CIN { get; set; }
+    public required string Phone { get; set; }
+    public Gender Gender { get; set; }
+    public string? Address { get; set; }
+    public string? Image { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public ulong UserId {  get; set; }
+    public required User User { get; set; }
+}
