03a74ca5faf33a2778cca14e63b67bcc8f44074f
Traam <younesMRX@outlook.com>
2024-12-29 17:23:47 +0100

    ✨ feat<application>: add LoginUserCommand and LoginUserCommandHandler Implemented LoginUserCommand for user login requests and LoginUserCommandHandler to process and handle the login logic.

---
+++ Application/Commands/Authentication/LoginUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using VetTail.Domain.Entities;
+
+namespace VetTail.Application.Commands.Authentication;
+
+public record LoginUserCommand(string Username, string Password) : IRequest<User>;
---
+++ Application/Commands/Handlers/LoginUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using VetTail.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Authentication;
+using VetTail.Domain.Common.Exceptions;
+using VetTail.Application.Commands.Authentication;
+
+namespace VetTail.Application.Commands.Handlers;
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, User>
+{
+    private readonly UserManager<User> userManager;
+    private readonly SignInManager<User> signInManager;
+
+    public LoginUserCommandHandler(UserManager<User> userManager, SignInManager<User> signInManager)
+    {
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+    }
+
+    public async Task<User> Handle(LoginUserCommand request, CancellationToken cancellationToken = default)
+    {
+        User user = await this.userManager.FindByNameAsync(request.Username) ?? throw new EntityNullReferenceException<User>(request.Username, nameof(request.Username));
+        SignInResult attempt = await this.signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+        if(!attempt.Succeeded) throw new InvalidCredentialException("Invalid Credentials");
+        return user;
+    }
+}
