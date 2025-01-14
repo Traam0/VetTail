7ce05e274dba32b9dda7c2074ebac80e76c81639
Traam0 <younesMRX@outlook.com>
2025-01-14 04:03:45 +0000

    ✨ feat<application>: add product and category to DTO Automapper profiles

---
+++ Application/Common/Profiles/CategoryProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VetTail.Application.Common.DTOs;
+using VetTail.Domain.Entities;
+
+namespace VetTail.Application.Common.Profiles;
+
+public class CategoryProfiles : Profile
+{
+    public CategoryProfiles()
+    {
+        CreateMap<Category, CategoryBrief>();
+
+    }
+}
---
+++ Application/Common/Profiles/ProductProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VetTail.Domain.Entities;
+using VetTail.Application.Common.DTOs;
+
+namespace VetTail.Application.Common.Profiles;
+
+public class ProductProfiles : Profile
+{
+    public ProductProfiles()
+    {
+        CreateMap<Product, ProductBrief>();
+    }
+}
